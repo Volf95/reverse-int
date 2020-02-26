4408403e799a9870ee66b8a53b22a60ffcb64b36
Laziz Khudayberdiev <57383456+Volf95@users.noreply.github.com>
2020-02-26 11:40:52 +0500

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let positiveNumber = Math.abs(n);
+    let result = positiveNumber.toString().split('').reverse().join('');
+    return Number(result);
 }
